58c8cb15b6428f4c6b601f6f2991775004ef7f0c
ll_admin <123@qq.com>
2018-09-14 17:17:34 +0800

    Ready for deployment to heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -137,12 +137,13 @@ if os.getcwd() == '/app':
     DATABASES = {
         'default':dj_database_url.config(default='postgres://localhost')
     }
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     #让request.is_secure()承认X_Forwarded_Proto头
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
     ALLOWED_HOSTS = ['*']
+
     #静态资产配置
-    BASE_DIR = os.path.dirname(os.path.abspath(_file_))
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
